ebf8862a482eb534ecc4623ac1c14c5766f55cad
moviewhisper <holganicbear@gmail.com>
2020-01-01 18:43:37 +0900

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ul>
+    <li class="question" @click="clickQuestion">出題</li>
+    <li class="correct" @click="clickCorrect">正解</li>
+    <li class="wrong" @click="clickWrong">不正解</li>
+  </ul>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Howl } from 'howler'
+import BuzzerQuestion from "../assets/buzzerQuestion.mp3"
+import BuzzerCorrect from "../assets/buzzerCorrect.mp3"
+import BuzzerWrong from "../assets/buzzerWrong.mp3"
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data: function() {
+    return {
+      buzzerQuestion: null,
+      buzzerCorrect: null,
+      buzzerWrong: null,
+      date: null,
+    }
+  },
+  methods: {
+    clickQuestion() {
+      this.buzzerQuestion.play()
+    },
+    clickCorrect() {
+      this.buzzerCorrect.play()
+    },
+    clickWrong() {
+      this.buzzerWrong.play()
+    },
+  },
+  mounted () {
+    this.buzzerQuestion = new Howl({ src: [BuzzerQuestion] })
+    this.buzzerCorrect = new Howl({ src: [BuzzerCorrect] })
+    this.buzzerWrong = new Howl({ src: [BuzzerWrong] })
+  },
 }
 </script>
+
+<style>
+ul {
+  margin: 0;
+  padding-left: 0;
+  display: flex;
+  width: 100%;
+  margin: 10px auto;
+}
+li {
+  list-style: none;
+  font-family: 'Lato', 'Noto Sans JP', '游ゴシック Medium', '游ゴシック体', 'Yu Gothic Medium', YuGothic, 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+  width: 100%;
+  margin: 0 5px;
+  color: white;
+  line-height: 90vh;
+  font-size: 2rem;
+}
+.question {
+  background: blue;
+}
+.correct {
+  background: green;
+}
+.wrong {
+  background: red;
+}
+</style>
+
